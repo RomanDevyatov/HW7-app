c542556fc0fb4d46c2c9e7c9cb3aa8f03aabb85a
RomanDevyatov <44396617+RomanDevyatov@users.noreply.github.com>
2020-01-11 22:21:55 +0300

    Update CustomerRepository.java

--- src/com/netcracker/repository/CustomerRepository.java
+++ src/com/netcracker/repository/CustomerRepository.java
@@ -13,11 +13,17 @@ import java.util.List;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     List<Customer> findByFamily(String family);
     List<Customer> findByGeography(String geography);
-    List<Customer> findByDiscount(String discount);
+    List<Customer> findByDiscount(double discount);
 
     void deleteById(long id);
 
-//    @Query(value = "SELECT * FROM  customer c WHERE c.family = :family", nativeQuery = true)
-//    List<Customer> retrieveByFamily(@Param("family") String family);
+    @Query(value = "SELECT * FROM  customer where customer.family = :family", nativeQuery = true)
+    List<Customer> retrieve(@Param("family") String family);
 
+
+    @Query(value = "select distinct c.geography from customer c", nativeQuery = true)
+    List<String> retrieveGeography();
+
+    @Query(value =  "select c.family,c.discount from customer c where c.geography='Нижегородском'", nativeQuery = true)
+    List<String> retrieveFamilyDiscount();
 }
