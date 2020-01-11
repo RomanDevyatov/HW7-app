639712265e0e5b060043422d916ff031dd6ba8dd
RomanDevyatov <44396617+RomanDevyatov@users.noreply.github.com>
2020-01-11 22:21:41 +0300

    Update BookRepository.java

--- src/com/netcracker/repository/BookRepository.java
+++ src/com/netcracker/repository/BookRepository.java
@@ -3,6 +3,8 @@ package com.netcracker.repository;
 import com.netcracker.entity.Book;
 import com.netcracker.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
@@ -12,4 +14,15 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByCost(int cost);
     List<Book> findByWareHouse(String wareHouse);
     List<Book> findByAmount(int amount);
+
+
+//    @Query(value = "select distinct name, cost from book;", nativeQuery = true)
+//    List<String> getNameCost(@Param("name") String name,
+//                           );
+
+    @Query(value = "select distinct name,cost from book", nativeQuery = true)
+    List<String> retrieveNameCost();
+
+    @Query(value = "select b.name,b.cost from book b where b.name like '%Windows%' or b.cost >20000 order by 1, 2 desc", nativeQuery = true)
+    List<String> retrieveNameLikeWindows();
 }
